12294df4774791c9a6c9d3839a5199f977f009ab
Jackie Lin <jlin00@stuy.edu>
2020-02-23 21:25:03 -0500

    edited main

--- main.c
+++ main.c
@@ -14,12 +14,12 @@ int main() {
   struct matrix *identity;
   struct matrix *rand;
 
-  printf("\nEDGES MATRIX\n");
+  printf("\nEDGES MATRIX (1, 2, 3), (4, 5, 6)\n");
   edges = new_matrix(4, 4);
   add_edge(edges, 1, 2, 3, 4, 5, 6);
   print_matrix(edges);
 
-  printf("\nIDENTITY MATRIX\n");
+  printf("\n4x4 IDENTITY MATRIX\n");
   identity = new_matrix(4, 4);
   ident(identity);
   print_matrix(identity);
@@ -28,7 +28,7 @@ int main() {
   matrix_mult(identity, edges);
   print_matrix(edges);
 
-  printf("\nRANDOM MATRIX\n");
+  printf("\nRANDOM MATRIX (3, 4, 8), (1, 0, 7), (1, 0, 7), (9, 1, 2)\n");
   rand = new_matrix(4, 4);
   add_edge(rand, 3, 4, 8, 1, 0, 7);
   add_edge(rand, 1, 0, 7, 9, 1, 2);
@@ -38,7 +38,7 @@ int main() {
   matrix_mult(rand, edges);
   print_matrix(edges);
 
-  printf("\nADDING MORE EDGES\n");
+  printf("\nADDING MORE EDGES (3, 3, 3), (4, 6, 8), (0, 0, 0), (9, 9, 9), (8, 8, 8), (1, 0, 2)\n");
   add_edge(edges, 3, 3, 3, 4, 6, 8);
   add_edge(edges, 0, 0, 0, 9, 9, 9);
   add_edge(edges, 8, 8, 8, 1, 0, 2);
@@ -47,4 +47,6 @@ int main() {
   free_matrix(edges);
   free_matrix(identity);
   free_matrix(rand);
+
+
 }
